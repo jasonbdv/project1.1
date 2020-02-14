12d4740c9f40d6bccc5fde14dd1276e61c655c5e
Jason Verde <jasonbenedict.verde.iics@ust.edu.ph>
2020-02-14 14:28:04 +0800

    commit with oop jscript

---
+++ index.js
@@ -0,0 +1,10 @@
+//object creation using object literal
+
+let juan = {name: "juan dela cruz", age: "20", school:"UST", 
+speak:function(language){
+    document.write(`${this.name} can speak ${language}`);
+}}
+
+juan.speak("ENGLISH");
+
+let x = 6;
