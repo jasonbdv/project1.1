5d21cab8b2870f3931d569a7b58239185ddd2031
Jason Verde <jasonbenedict.verde.iics@ust.edu.ph>
2020-02-14 14:51:34 +0800

    ES6

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//object creation using constructor function
+//object creation using ECMAScript 6 (ES6) syntax
 
-function Student(name, section){
-    this.name = name;
-    this.section = section;
-    this.speak = function(language){
-        document.write(`${this.name} can speak ${language}`);
+class Student{
+    constructor(name, course, section){
+        this.name = name;
+        this.course = course;
+        this.section = section;
     }
 
+    speak = function(language){
+        document.write(`${this.name} can speak ${language}`)
+    }
 }
 
-let stud1 = new Student("Juan Dela Cruz", "2ITA");
-stud1.speak("chingchong");
+let stud1 = new Student("Juan Dela Cruz", "IT", "2ITA");
+let stud2 = new Student("Pedro Hernandez", "CS", "2CSB");
+
+stud2.speak("Visaya");
